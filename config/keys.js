35d705171c05ed9c6cdd4be7f45a344ad089c315
= <=>
2021-11-12 12:45:37 -0600

    updated passport setup

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+// add to gitignore
+
+module.exports = {
+    google:{
+        clientID:"792894982295-i71tr1mmrv290823sjas8fb1rjj50rse.apps.googleusercontent.com",
+        clientSecret: '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'
+    }
+};
---
+++ config/passport-setup.js
@@ -0,0 +1,30 @@
+//call passport 
+const passport = require('passport');
+
+//call google strategy
+const googleStrategy = require('passport-google-oauth');
+
+//call facebook strategy
+const facebookStrategy = require('passport-facebook');
+
+const keys = require('./keys');
+
+
+passport.use(
+    new googleStrategy({
+        //options for the strategy
+        clientID: keys.google.clientID,
+        clientSecret: keys.google.clientSecret
+    }), () => {
+        //passport callback function
+    }
+);
+
+passport.use(
+    new facebookStrategy({
+
+    }), () => {
+        //passport callback function
+    }
+)
+
