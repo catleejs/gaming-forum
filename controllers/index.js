a5eb87305a77c47f81ac84962979840593a11d37
= <=>
2021-11-18 12:50:12 -0600

    updating routes

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,12 @@ const router = require("express").Router();
 
 const apiRoutes = require("./api");
 const homeRoutes = require("./home-routes.js");
+const authRoutes = require('./auth-routes');
 
 router.use("/", homeRoutes);
 
 router.use("/api", apiRoutes);
+router.use('/auth', authRoutes)
 
 router.use((req, res) => {
   res.status(404).end();
